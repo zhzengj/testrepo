b451a4386d248be689a39b1a9499cc039f691f82
unknown <zhzengj@.(none)>
2011-10-14 19:13:24 +0800

    weather add tips

--- coding/qt/weather/weather.cpp
+++ coding/qt/weather/weather.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 #include "weather.h"
+#include <QtGui/QtGui>
 
 weather::weather(QWidget *parent, Qt::WFlags flags)
-	: QWidget(parent, flags)
+: QWidget(parent, flags)
 {
 	ui.setupUi(this);
 }
@@ -11,3 +12,39 @@ weather::~weather()
 {
 
 }
+
+ZWeatherTrayIcon::ZWeatherTrayIcon(QObject *parent /* = 0 */)
+:QSystemTrayIcon(parent)
+{
+	setIcon(QIcon(":/weather/Resources/weather.gif"));
+	connect(this, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+		this, SLOT(onActivated(QSystemTrayIcon::ActivationReason)));
+}
+
+void ZWeatherTrayIcon::onActivated(QSystemTrayIcon::ActivationReason reason)
+{
+	QRect rt = geometry();
+	QWidget * tips = new weather(NULL, Qt::Popup);
+	tips->setAttribute(Qt::WA_DeleteOnClose);
+	tips->move(rt.left(), rt.top() - tips->height());
+	tips->show();
+}
+
+bool ZWeatherTrayIcon::event(QEvent *e)
+{
+	if (e->type() == QEvent::ToolTip)
+	{
+		QHelpEvent * he = dynamic_cast<QHelpEvent*> (e);
+		if (he)
+		{
+			QWidget * tips = new weather(NULL, Qt::Popup);
+			tips->setAttribute(Qt::WA_DeleteOnClose);
+			tips->move(he->globalPos());
+			tips->show();
+		}
+		return true;
+	}
+	return false;
+}
+
+
--- coding/qt/weather/weather.h
+++ coding/qt/weather/weather.h
@@ -2,6 +2,7 @@
 #define WEATHER_H
 
 #include <QtGui/QWidget>
+#include <QtGui/QSystemTrayIcon>
 #include "ui_weather.h"
 
 class weather : public QWidget
@@ -21,5 +22,11 @@ class ZWeatherTrayIcon: public QSystemTrayIcon
 	Q_OBJECT
 public:
 	ZWeatherTrayIcon(QObject *parent = 0);
+
+protected slots:
+	void onActivated(QSystemTrayIcon::ActivationReason reason);
+
+protected:
+	virtual bool event(QEvent *e);
 };
 #endif // WEATHER_H
